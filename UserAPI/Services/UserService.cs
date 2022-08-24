5e7c56b55e8c8e491542fc936be5d197d72f1290
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-24 14:26:27 -0500

    user service added

---
+++ UserAPI/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using UserAPI.Data;
+using UserAPI.Models;
+
+namespace UserAPI.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public User AddUser(User product)
+        {
+            var result = context.Users.Add(product);
+            context.SaveChanges();
+            return result.Entity;
+        }
+
+        public bool DeleteUser(int Id)
+        {
+            var filteredData = context.Users.Where(x => x.UserId == Id).FirstOrDefault();
+            var result = context.Remove(filteredData);
+            context.SaveChanges();
+            return result != null ? true : false;
+        }
+
+        public User GetUserById(int id)
+        {
+            return context.Users.Where(x => x.UserId == id).FirstOrDefault();
+        }
+
+        public IEnumerable<User> GetUserList()
+        {
+            return context.Users.ToList();
+        }
+
+        public User UpdateUser(User product)
+        {
+            var result = context.Users.Update(product);
+            context.SaveChanges();
+            return result.Entity;
+        }
+    }
+}
