4678978e7b34335de2e5ec91cc801278e6d36860
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-24 12:21:04 -0500

    applicationdbcontext class added

---
+++ UserAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAPI.Models;
+
+namespace UserAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        protected ApplicationDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
